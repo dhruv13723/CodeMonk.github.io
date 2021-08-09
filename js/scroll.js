71c2dc6e3a4f479953ffc81d6e919c752fa894f6
dhruv13723 <49592456+dhruv13723@users.noreply.github.com>
2021-08-09 10:00:19 +0530

    CodeMonk Website

---
+++ js/scroll.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $(".goTop").fadeIn();
+        }
+        else {
+            $('.goTop').fadeOut();
+        }
+    })
+
+    $('.goTop').click(function(){scroll(0, 0)});
+})
